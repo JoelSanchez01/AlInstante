9cdf01a26876194d48db17d2ce0b0b0e489fd60e
citlalicstll <castillo.carreon.19090@itsmante.edu.mx>
2023-04-19 00:26:58 -0600

    Se hizo la pantallita de visualizar contenido

--- src/app/inside/page.jsx
+++ src/app/inside/page.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import './NewsViewCard.css'
+import Image from "next/image";
 
 function InsidePage() {
   return (
-    <div>
-      <h1>Inside Page</h1>
-    </div>
+    <div className="news-view-frame">
+      <article className='news-view-card'>
+            <div className='news-view-card-info'>
+                <span className='news-view-card-title'>Titulo noticia - Sección</span>
+            </div>
+            <aside>
+                <Image className="news-view-card-edit" src="/edit_icon.png" width={30} height={30}/>
+                <Image className="news-view-card-delete" src="/delete_icon.png" width={30} height={30}/>
+            </aside>
+    </article>
+    </div> 
   );
 }
 
