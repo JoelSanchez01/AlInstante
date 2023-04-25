0859d43308fdbe1f235d8b6ddf69f95c432d1d6f
Joel Sanchez <joeleduardosanchezherrera@gmail.com>
2023-04-24 19:50:34 -0600

    se reviso y corrigio el login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,33 +1,28 @@
 import React from "react";
-import './styles.css';
-
-
-
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from './page.module.css'
+import "./styles.css";
 
 export default function Home() {
   return (
-  <div class ="box">
-  <form>
-      <h1>Al Instante</h1>
-      <h2>Iniciar Sesión</h2>
-      <div class = "inputBox">
-          <input type = "text" required ="required"/> 
+    <div class="box">
+      <form>
+        <h1>Al Instante</h1>
+        <h2>Iniciar Sesión</h2>
+        <div class="inputBox">
+          <input type="text" required="required" />
           <span>Usuario</span>
           <i></i>
-      </div>
-      <div class="inputBox">
-          <input type = "password" required ="required"/> 
+        </div>
+        <div class="inputBox">
+          <input type="password" required="required" />
           <span>Contraseña</span>
           <i></i>
-      </div>
-  <div class="links">
-      <a href="#">¿Olvidaste la contraseña?</a>
-      <a href="#">Registrarse</a>
-  </div>
-  <input type = "submit" value="Login" />
-  </form>
-</div>) 
+        </div>
+        <div class="links">
+          <a href="#">¿Olvidaste la contraseña?</a>
+          <a href="#">Registrarse</a>
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
 }
