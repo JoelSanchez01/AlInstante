e19258df34fc8644dceb2ce523ee22b91625763b
DamarisUwU <guevara.hernandez.19041@itsmante.edu.mx>
2023-04-24 19:39:54 -0600

    Corregí un poco el diseño del login

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -1,11 +1,33 @@
 import React from "react";
+import './styles.css';
 
-function LoginInde() {
+
+
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from './page.module.css'
+
+export default function Home() {
   return (
-    <div>
-      <h1>Login Page</h1>
-    </div>
-  );
+  <div class ="box">
+  <form>
+      <h1>Al Instante</h1>
+      <h2>Iniciar Sesión</h2>
+      <div class = "inputBox">
+          <input type = "text" required ="required"/> 
+          <span>Usuario</span>
+          <i></i>
+      </div>
+      <div class="inputBox">
+          <input type = "password" required ="required"/> 
+          <span>Contraseña</span>
+          <i></i>
+      </div>
+  <div class="links">
+      <a href="#">¿Olvidaste la contraseña?</a>
+      <a href="#">Registrarse</a>
+  </div>
+  <input type = "submit" value="Login" />
+  </form>
+</div>) 
 }
-
-export default LoginInde;
