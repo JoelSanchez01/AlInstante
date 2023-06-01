7399538f840b1be9592594525600941380310be5
DiegoCass01 <diego.castillo170901@gmail.com>
2023-06-01 01:06:12 -0600

    variable de entorno

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,11 +1,10 @@
-import mongoose from 'mongoose'
+import mongoose from "mongoose";
 
 export async function connectToDB() {
   try {
-    await mongoose.connect("mongodb+srv://DiegoCass:diego123@cluster0.4oc98cj.mongodb.net/?retryWrites=true&w=majority")
-    console.log('Mongodb connected')
+    await mongoose.connect(MONGO);
+    console.log("Mongodb connected");
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
-  
 }
