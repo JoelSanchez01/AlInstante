f0dc171b9df010c8f3d5d516a46f90a7edcad348
DiegoCass01 <diego.castillo170901@gmail.com>
2023-06-01 02:05:34 -0600

    entorno de produccion

--- utils/mongoose.js
+++ utils/mongoose.js
@@ -1,8 +1,11 @@
 import mongoose from "mongoose";
+const dev_db_url =
+  "mongodb+srv://DiegoCass:diego123@cluster0.4oc98cj.mongodb.net/?retryWrites=true&w=majority";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 export async function connectToDB() {
   try {
-    await mongoose.connect("mongodb+srv://DiegoCass:diego123@cluster0.4oc98cj.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(mongoDB);
     console.log("Mongodb connected");
   } catch (error) {
     console.error(error);
